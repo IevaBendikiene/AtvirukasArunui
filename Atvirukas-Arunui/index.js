2f96e6d630e7b717550413b0c9e4634e41131522
IevaBendikiene <126477799+IevaBendikiene@users.noreply.github.com>
2023-03-03 15:54:52 +0200

    Update index.js

--- Atvirukas-Arunui/index.js
+++ Atvirukas-Arunui/index.js
@@ -4,7 +4,6 @@ const jsConfetti = new JSConfetti()
 document.getElementById("button").addEventListener("click",fireConfetti)
 
 function fireConfetti() {
-
     jsConfetti.addConfetti({
         confettiRadius:1,
         confettiNumber: 100,
